f8ef511eba2b5ead3c2fc4f9ad97766fdb39dc5b
Matthew Thomson <m7homson@gmail.com>
2017-03-26 18:23:06 -0400

    Hacked together the manual trigger and dynamic scrolling.

--- app/src/main/java/mthomson/coneath/MainActivity.java
+++ app/src/main/java/mthomson/coneath/MainActivity.java
@@ -1,12 +1,16 @@
 package mthomson.coneath;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,15 +20,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        graph = (GraphView) findViewById(R.id.graph);
+        graph.getViewport().setMaxX(20.0);
+        graph.getViewport().setXAxisBoundsManual(true);
+        update_graph();
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, ping()),
-                new DataPoint(1, ping()),
-                new DataPoint(2, ping()),
-                new DataPoint(3, ping())
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                update_graph();
+            }
         });
+
+    }
+    private GraphView graph;
+
+    private ArrayList<DataPoint> dataPoints = new ArrayList<>();
+    private double datapoint_num = 0.0;
+
+    private void update_graph() {
+        dataPoints.add(new DataPoint(datapoint_num, ping()));
+        datapoint_num+=1.0;
+        graph.removeAllSeries();
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPoints.toArray(new DataPoint[dataPoints.size()]));
         graph.addSeries(series);
+        graph.getViewport().scrollToEnd();
     }
 
     private double ping() {

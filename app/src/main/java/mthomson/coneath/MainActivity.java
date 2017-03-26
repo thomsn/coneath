65477b474e14f89b71fc2d05c7375d2280db18b1
Matthew Thomson <m7homson@gmail.com>
2017-03-26 17:11:25 -0400

    Hacked together the ping functionality.

--- app/src/main/java/mthomson/coneath/MainActivity.java
+++ app/src/main/java/mthomson/coneath/MainActivity.java
@@ -1,10 +1,14 @@
 package mthomson.coneath;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +19,44 @@ public class MainActivity extends AppCompatActivity {
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(0, ping()),
+                new DataPoint(1, ping()),
+                new DataPoint(2, ping()),
+                new DataPoint(3, ping())
         });
         graph.addSeries(series);
+    }
+
+    private double ping() {
+        Runtime runtime = Runtime.getRuntime();
+        try
+        {
+            Process  process = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int mExitValue = process.waitFor();
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            if(mExitValue==0){
+                Pattern pattern = Pattern.compile("time=(\\d.+)*\\sms");
+                Matcher m;
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    m = pattern.matcher(inputLine);
+                    if (m.find()) {
+                        return Double.parseDouble(m.group(1));
+                    }
+                }
+            }else{
+                BufferedReader error = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+                String error_str = error.readLine();
+                System.out.println(" mExitValue "+ error_str);
+                return -1.0;
+            }
+        }
+        catch (InterruptedException | IOException ignore)
+        {
+            ignore.printStackTrace();
+            System.out.println(" Exception:"+ignore);
+        }
+        return -1.0;
     }
 }
